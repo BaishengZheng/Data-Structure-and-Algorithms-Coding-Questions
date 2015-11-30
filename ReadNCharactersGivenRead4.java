03188518d455a427fd44090c42982c0ecebc24db
BaishengZheng <baisheng@engin.umass.edu>
2015-11-29 19:54:21 -0500

    Update ReadNCharactersGivenRead4.java

--- ReadNCharactersGivenRead4.java
+++ ReadNCharactersGivenRead4.java
@@ -2,6 +2,8 @@
 The API: int read4(char *buf) reads 4 characters at a time from a file and save read characters into buf.
 The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
 By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+Note:
+The read function will only be called once for each test case.
 */
 
 /* The read4 API is defined in the parent class Reader4.
